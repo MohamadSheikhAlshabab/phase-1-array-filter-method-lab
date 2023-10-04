c5fff41466d5d9b0f1f4e7f9388e84ca4a027fa3
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-04 19:55:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arrayOfNames, name){
+    return arrayOfNames.filter(element => element.slice(1) === name.slice(1));
+}
+
+function fuzzyMatch(arrayOfNames, name){
+    return arrayOfNames.filter(element => element.slice(0,2) === name);
+}
+
+function matchName(arrayOfNames, name){
+    return arrayOfNames.filter(element => element.name === name);
+}
